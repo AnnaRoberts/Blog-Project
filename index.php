96ed2af261527c18e1817a2b95382486be5f9ea1
Anna Roberts <annaroberts960608@gmail.com>
2016-03-21 12:08:39 -0400

    adjust the blank template layout to work for blog site and upload them to wordpress

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <?php get_header(); ?>         
                 
-			<section class="row">
-                <div class="twelve columns">              
-                	<?php
-                    if ( have_posts() ) {
-                        while ( have_posts() ) {
-                        	the_post(); ?>
-                        	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                        	<?php if ( has_post_thumbnail() ) {
-                                the_post_thumbnail('thumbnail');
-                            } ?>
-                            <?php the_excerpt();?>
-                        	<a href="<?php the_permalink(); ?>">Read More...</a>
-                        <?php } // end while                        		 
-                    } //end if
-                    ?>
-                </div>
-            </section>
+        <section class="row">
+            <div class="nine columns">              
+                <?php
+                if ( have_posts() ) {
+                    while ( have_posts() ) {
+                        the_post(); ?>
+                        	
+                    <div class="row">
+                        <div class="three columns">
+                            <?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');} ?>
+                        </div>
+                        <div class="nine columns">
+                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                             <?php the_excerpt();?>
+                             <a href="<?php the_permalink(); ?>">Read More...</a>
+                        </div>
+                    </div>
+
+                    <?php } // end while                        		 
+                } //end if
+                ?>
+            </div>
+            <div class "three columns">
+                 <h3>SideBar</h3>
+            </div>
+        </section>
                     
  <?php get_footer(); ?>
