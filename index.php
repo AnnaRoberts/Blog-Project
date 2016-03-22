f84c707c758f5291da5be87818628bb69fcf5ab5
Anna Roberts <annaroberts960608@gmail.com>
2016-03-22 11:26:43 -0400

    adjust widths to match the mockup. Tried to get them to display inline and could not do it.

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>         
                 
         <section class="row">
-            <div class="nine columns">              
+            <div class="nine columns">               
                 <?php
                 if ( have_posts() ) {
                     while ( have_posts() ) {
                         the_post(); ?>
                         	
                     <div class="row">
-                        <div class="three columns">
-                            <?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');} ?>
-                        </div>
-                        <div class="nine columns">
-                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                             <?php the_excerpt();?>
-                             <a href="<?php the_permalink(); ?>">Read More...</a>
+                        <div class="blog-displays">
+                            <div class="nine columns">
+                                <?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');} ?>
+                            </div>
+                            <div class="nine columns">
+                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                 <?php the_excerpt();?>
+                                 <a href="<?php the_permalink(); ?>">Read More...</a>
+                            </div>
                         </div>
                     </div>
-
+            
 
                     <?php } // end while                        		 
                 } //end if
