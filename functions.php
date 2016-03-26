357d28af63fdef1c4e53b9288e70e38e3d306597
Anna Roberts <annaroberts960608@gmail.com>
2016-03-26 17:34:02 -0400

    add files for jQuery plugin

--- functions.php
+++ functions.php
@@ -68,3 +68,31 @@ add_theme_support('menus');
 add_theme_support( 'post-thumbnails'); 
 
 ?>
+
+
+
+
+<?php
+/*--- Enable jQuery Plugin ---*/
+
+wp_deregister_script('jquery');
+
+wp_enqueue_script('jquery', 'http://code.jquery.com/jquery-latest.min.js','','',true);
+?>
+
+<?php
+/*--- Enable jQuery Plugin ---*/
+function enqueue_unslider() {
+    wp_enqueue_script( //function to enqueue script
+        'unslider', //name of our script (id)
+        get_template_directory_uri() . '/js/unslider.js', //file loc
+        array('jquery'), //dependencies
+        '', //version - left blank
+        true //load in footer
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_unslider');
+?>
+
+
+
--- index.php
+++ index.php
@@ -7,6 +7,13 @@
             <div class "six columns">
                 <?php dynamic_sidebar('slideshow'); ?>
             </div>
+            <div class="my-slider">
+                <ul>
+                    <li>My slide</li>
+                    <li>Another slide</li>
+                    <li>My last slide</li>
+                </ul>
+            </div>
             <div class="nine columns">               
                 <?php
                 if ( have_posts() ) {
@@ -15,10 +22,10 @@
                         	
                     <div class="row">
                         <div class="blog-displays">
-                            <div class="nine columns">
+                            <div class="twelve columns">
                                 <?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');} ?>
                             </div>
-                            <div class="nine columns">
+                            <div class="twelve columns">
                                 <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                                  <?php the_excerpt();?>
                                  <a href="<?php the_permalink(); ?>">Read More...</a>
@@ -33,5 +40,10 @@
             </div>
 
         </section>
+
+<script src="//code.jquery.com/jquery-2.1.4.min (1).js"></script>
+<script src="/js/unslider-master/src/js/unslider.js"></script> 
+<script src="js/main.js"></script>
+
                     
  <?php get_footer(); ?>
