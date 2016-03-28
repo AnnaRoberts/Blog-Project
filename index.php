f8f6216305481ae361ed7d16afc88fb1723f9548
Anna Roberts <annaroberts960608@gmail.com>
2016-03-28 10:36:57 -0400

    add four columns to blog post

--- index.php
+++ index.php
@@ -7,28 +7,29 @@
             <div class "six columns">
                 <?php dynamic_sidebar('slideshow'); ?>
             </div>
-            <div class="nine columns">               
+                          
                 <?php
                 if ( have_posts() ) {
                     while ( have_posts() ) {
                         the_post(); ?>
-                        	
-                    <div class="row">
-                        <div class="blog-displays">
-                            <div class="twelve columns">
-                                <?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');} ?>
-                            </div>
-                            <div class="twelve columns">
-                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                                 <?php the_excerpt();?>
-                                 <a href="<?php the_permalink(); ?>">Read More...</a>
+                    <div clas="four columns">    	
+                        <div class="row">
+                            <div class="blog-displays">
+                                <div class="twelve columns">
+                                    <?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');} ?>
+                                </div>
+                                <div class="twelve columns">
+                                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                     <?php the_excerpt();?>
+                                     <a href="<?php the_permalink(); ?>">Read More...</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <?php } // end while                        		 
                 } //end if
                 ?>
-            </div>
+            
         </section>
 
                     
