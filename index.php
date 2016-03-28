8906f9973bc8bdbc332047e3f50baa2f78910cf5
Anna Roberts <annaroberts960608@gmail.com>
2016-03-28 15:05:17 -0400

    got blogs to display inline (inline-flex + making 9 columns instead of 4)

--- index.php
+++ index.php
@@ -7,17 +7,17 @@
             <div class "six columns">
                 <?php dynamic_sidebar('slideshow'); ?>
             </div>
-                          
+                <div class="container-blog-displays">         
                 <?php
                 if ( have_posts() ) {
                     while ( have_posts() ) {
                         the_post(); ?>  	
                         <div class="row">
                             <div class="blog-displays">
-                                <div class="four columns">
+                                <div class="nine columns">
                                     <?php if ( has_post_thumbnail() ) { the_post_thumbnail('thumbnail');} ?>
                                 </div>
-                                <div class="four columns">
+                                <div class="nine columns">
                                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                                      <?php the_excerpt();?>
                                      <a href="<?php the_permalink(); ?>">Read More...</a>
@@ -27,6 +27,7 @@
                     <?php } // end while                        		 
                 } //end if
                 ?>
+                </div> 
             
         </section>
 
